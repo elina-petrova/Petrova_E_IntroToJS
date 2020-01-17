083f8b54bcf10d626e4969a72d364711b6ceb76e
Petrova <e_petrova@ats-hr420-dm06.fc.ca>
2020-01-17 13:44:31 -0500

    refreshedpage

--- js/main.js
+++ js/main.js
@@ -9,7 +9,9 @@
 
   let theButton = document.querySelector("#myButton"),
       theHeading = document.querySelector("h1"),
-      svgImage = document.querySelector("#svgGraphic");
+      svgImage = document.querySelector("#svgGraphic"),
+    //  svgImage2 = document.querySelector("#svgGraphic2");
+      allSVGs = document.querySelectorAll(".svg");
 
 
 //log the id of the svg to the console when a user hovers on it
@@ -21,8 +23,18 @@
     theHeading.textContent = "there is no way out of JS";
   }
 
+  for (el in allSVGs){
+
+  }
+
+
   theButton.addEventListener('click', changeText);
-  svgImage.addEventListener('mouseover', logSVG);
+  //svgImage.addEventListener('click', logSVG);
+  svgImage.addEventListener('click', changeSVG);
+  //svgImage2.addEventListener('click', logSVG);
+
+  //loop throuh the collection and apply out event handlign to each element
+  allSVGs.forEach(item => item.addEventListener('click', logSVG));
 
   //document.querySelector('h1').textContent = "there is no way out of JS";
 
